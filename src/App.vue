6c574b383b428dfce445bd94bf867b1bc7ff3148
mlazana <mariettalaz98@gmail.com>
2019-09-15 00:29:24 +0300

    Add dynamic background based on route

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,33 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+<div id="app">
+<div id="main" :class="routeClass">
+<router-view/>
+</div>
+</div>
 </template>
 
 <script>
-
 export default {
-  name: 'app'
+name: 'app',
+computed: {
+	routeClass() {
+		if (this.$route.name === "Home") {
+			return 'home-bg'
+		} else if (this.$route.name === "About") {
+			return 'about-bg'
+		} else if (this.$route.name === 'Contact') {
+			return 'contact-bg'
+		}
+		return '';
+	}
+}
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Roboto', sans-serif;
-  
-   
-
+font-family: 'Roboto', sans-serif;
+height: 100%;
+margin: 0;
 }
-
-
 </style>
